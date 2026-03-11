effd13a76a748ace6ac59c56bb242279c245dbbf
Ally7574 <allysonvictorsouza@gmail.com>
2026-03-11 20:55:37 -0300

    primeiro projeto

---
+++ src/Aluno.java
@@ -0,0 +1,13 @@
+public class Aluno {
+
+    //Definir o atributos do aluno
+    short idade;
+    String NomeDoAluno;
+    char Turma;
+    boolean matriculado;
+    int id;
+    String curso;
+    double Mensalidade;
+
+
+}
---
+++ src/Master.java
@@ -0,0 +1,19 @@
+public class Master {
+
+    public static void main(String[] args) {
+        System.out.println("Bom dia");
+
+        //Declarar variavel no java
+        short idade;
+        //atribuindo um valor para uma variavel
+        idade = 32;
+        //declarar a variavel e atribuir um valor
+        int anoDeNascimento = 1994;
+        double salario = 16000.53;
+        float altura = 16000.50f;
+        char inicialDoNome = 'L';
+        boolean maiorIdade = true; // ou false
+        String nome = "Lucas";
+
+    }
+}
